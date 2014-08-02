4b2f15b868482b55950e4ca0bb8f1e07266f9f09
Murat Knecht <murat.knecht@gmx.de>
2014-08-02 15:33:38 +1200

    cake-rows: Hough mapping with only existing angles. Solves the checkio task.

--- cakes-rows.py
+++ cakes-rows.py
@@ -5,20 +5,29 @@
 # https://www.cs.sfu.ca/~hamarneh/ecopy/compvis1999_hough.pdf
 
 from collections import defaultdict
-from math import cos, pi, sin
+from math import atan, cos, pi, sin
 
 precision = 1
 factor = 8
 
+grad = lambda a: a * 180 / pi
+
+
+def get_angles(p, others):
+    lineangle = lambda a, b: (
+        (atan(float(a[0] - b[0]) / (a[1] - b[1])))
+        if (a[1] - b[1]) != 0
+        else (pi / 2)
+    )
+    vectorangle = lambda angle: pi - ((pi / 2) - angle)
+    return {vectorangle(lineangle(p, o)) for o in others}
+
 
 def get_counter_array(points):
-    dimx = reduce(max, map(lambda p: p[0], points))
-    dimy = reduce(max, map(lambda p: p[1], points))
-    maxlines = (dimx + dimy) * factor
-    print("maxlines {}".format(maxlines))
     parameterspace = defaultdict(lambda: [0, []])
     for i, j in points:
-        for h in (float(h) / maxlines * pi for h in range(maxlines)):
+        others = set([tuple(p) for p in points]) - {(i, j)}
+        for h in get_angles((i, j), others):
             p = i * sin(h) + j * cos(h)
             parameterspace[(round(p, factor), h)][0] += 1
             parameterspace[(round(p, factor), h)][1] += [(i, j)]
@@ -38,9 +47,8 @@ def checkio(cakes):
     arr = get_counter_array(cakes)
     numlines = len(find_minimums(arr))
     print("All:")
-    # for k, v in sorted(arr.items()):
-    #     if (7, 5) in v[1]:
-    #         print("{} -> {}".format(v, k))
+    for k, v in sorted(arr.items()):
+        print("{} -> {}".format(v, k))
     # print("nulls:")
     # print([(k[0], v) for k, v in arr.items() if k[1] == 0])
     print(numlines)
@@ -49,6 +57,7 @@ def checkio(cakes):
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
+    # assert checkio([[4, 3], [6, 5], [9, 8], [3, 8], [9, 2]]) == 2
     assert checkio([[3, 3], [5, 5], [8, 8], [2, 8], [8, 2]]) == 2
     assert checkio(
         [[2, 2], [2, 5], [2, 8], [5, 2], [7, 2], [8, 2],
